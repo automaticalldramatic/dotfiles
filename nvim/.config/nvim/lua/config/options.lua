f74eba024fcef85f5be4e50916e645438fd52baf
riz <riz@agilefoxhq.com>
2025-12-01 22:33:15 +0100

    feat(nvim): Configure tab and indentation settings
    - Neovim display options changed for Go, following convention, showing tabs as 4 spaces wide and keeping 1 tab per indentation level

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,5 +3,11 @@
 -- Add any additional options here
 
 -- Show hidden files and folders in netrw explorer
-vim.g.netrw_hide = 0  -- 0 = show hidden files, 1 = hide hidden files
-vim.g.netrw_list_hide = ""  -- clear any hide patterns
+vim.g.netrw_hide = 0 -- 0 = show hidden files, 1 = hide hidden files
+vim.g.netrw_list_hide = "" -- clear any hide patterns
+
+-- Indentation and tab display settings
+vim.opt.tabstop = 4 -- Display tabs as 4 spaces wide
+vim.opt.shiftwidth = 1 -- Keep 1 tab per indentation level (Go convention)
+vim.opt.expandtab = false -- Use tabs, not spaces
+vim.opt.softtabstop = 4 -- Make backspace delete 4 spaces worth of tab
